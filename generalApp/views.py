d13e4ef2100d6fdaf85d5973bbe0221e93fda8b3
emberWorm <wrie.cope@gmail.com>
2025-03-03 21:35:33 +0300

    test update db if title post == new title

--- generalApp/views.py
+++ generalApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . import models, forms
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -29,16 +30,38 @@ def insta_post(request, pk):
 
 @login_required(login_url='users:login')
 def create_post(request):
-    form = forms.PostCreateForm # обязательно ли () - ?
+    form = forms.PostCreateForm() # обязательно ли () - ?
+
+    message = '' # объявляем переменную чтобы не было исключений
 
     if request.method == "POST":
         form = forms.PostCreateForm(request.POST,request.FILES) # насколько я помню он славливает ее не сохраняя
         if form.is_valid():
-            form.save()
-            return redirect('generalApp:url_profile')
+            title = form.cleaned_data['title'] # Данные в cleaned_data уже прошли все проверки is_valid() (заполнение, тип, формат даты..)
+            body = form.cleaned_data['body']
+            image = form.cleaned_data['image']
+            if models.Post.objects.filter(title=title).exists():
+                post = models.Post.objects.get(title=title)
+                post.body = body
+                post.image = image
+                post.save() #---?
+
+                message = 'поскольку был пост, обновлен былЪ'
+                # messages.info(request, 'Пост с таким названием уже сущетсвует') - для админки
+
+            # title=title вытащи среди всех названий постов имя этого
+            # exists() - дает результат bool
+
+            else:
+                form.save()
+                return redirect('generalApp:url_home')
+
         
     context = {
-        "form": form
+        "form": form,
+        "message":message,
     }
         
     return render (request, 'generalApp/create_post.html', context)
+
+                # models.Post.objects.create(title=title, body=body, image=image)
